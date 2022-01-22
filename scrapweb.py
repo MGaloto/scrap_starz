8a789f0ec84b3cfb067c83e2a96a4e442b93480a
MGaloto <maxigaloto@hotmail.com>
2022-01-22 14:45:33 -0300

    Series

--- scrapweb.py
+++ scrapweb.py
@@ -33,9 +33,6 @@ PLUS: Identificar modelo de negocio
 html = driver.execute_script('return document.documentElement.outerHTML')
 dom = BS(html, 'html.parser')
 
-# series = dom.find_all('h3', class_='title text-center on-hover')
-
-# nombre_serie = []
 
 links = dom.find_all('starz-content-item')
 
@@ -47,6 +44,9 @@ tabla_series = [['Nombre','Año', 'Episodios', 'Tipo', 'Link', 'Sinopsis']]
 
 #%%
 
+diccionario = []
+
+series_temporadas = []
 
 
 count = 0
@@ -68,37 +68,68 @@ for i in range(len(links)):
     tipo = datos.find('li').find_next_sibling("li").find_next_sibling("li").get_text()
     año = datos.find('li').find_next_sibling("li").find_next_sibling("li").find_next_sibling("li").get_text()
     sinopsis = dom.find('p', lines='3').text
-    variables = [nombre, año, episodios, tipo, url_final, sinopsis]
-    tabla_series.append(variables)
     print(count, '----',nombre,  '--', sinopsis)
+    caracteristicas = {'Nombre': nombre, 'Año': año, 'Episodios':episodios, 'Tipo':tipo, 'Link':url_final, 'Sinopsis':sinopsis}
+    diccionario.append(caracteristicas) 
     break
     
     
-    
-    
-#%%
+            
     
 
-with open('series.csv', 'w', newline = '') as series:
-     salida = csv.writer(series)
-     salida.writerows(tabla_series)
-
-
-
+    
 
 #%%
-    
-# Prueba
+
+
+# Prueba Series
 
 options = webdriver.ChromeOptions()
 options.add_argument("--incognito")
 driver = webdriver.Chrome('chromedriver.exe', options = options)
-url = 'https://www.starz.com/ar/es/series/express/63120'
+url = 'https://www.starz.com/ar/es/series/the-borgias/61183'
 driver.get(url)
+
+sleep(5)
 
 html = driver.execute_script('return document.documentElement.outerHTML')
 
 dom = BS(html, 'html.parser')
+
+
+
+
+ver_temporadas = dom.find('div', attrs = {'class': "season-selector d-flex"})
+ver_temporada = ver_temporadas.find('div', attrs = {'class': "season-number d-inline-block text-center active"}).text
+
+
+
+series_temporadas = []
+
+if ver_temporada == '1':
+    for i in range(len(ver_temporadas)):
+        sleep(1)
+        url_temporadas = ver_temporadas.find_all('a')[i]['href']
+        url_temporadas_final = 'https://www.starz.com' + url_temporadas
+        print(url_temporadas_final)
+        driver.get(url_temporadas_final)
+        sleep(3)
+        html = driver.execute_script('return document.documentElement.outerHTML')
+        dom = BS(html, 'html.parser')
+        sleep(6)
+        temporada = i + 1
+        capitulos_div = dom.find_all('div', attrs = {'class': "col-12 col-sm-6 col-lg-9 align-self-center"})  
+        for capitulo in capitulos_div:
+            nombre_capitulo = capitulo.find('h6', attrs = {'class': "title"}).text
+            temporada = temporada
+            minutos = capitulo.find('li').find_next_sibling("li").text
+            descripcion = capitulo.find('p', lines='2').text
+            caracteristicas_temporadas = {'Temporada': temporada, 'Nombre': nombre_capitulo, 'Duracion' : minutos, 'Descripcion' : descripcion}
+            series_temporadas.append(caracteristicas_temporadas) 
+            
+
+            
+
 
 
 
@@ -118,24 +149,14 @@ coleccion = bd['series']
 #%%
 
 
-
-
-
-
-
-
-#%%
-
 # Insertar datos
 
-coleccion.insert_many('poner la lista que contiene el diccionario')
+coleccion.insert_many(diccionario)
 print('datos subidos')
 
 
+
 #%%
-
-
-
 
 
 
