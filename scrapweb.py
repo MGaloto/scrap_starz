6bc37033d4eb10da895ec1ce94a43d818adb8869
MGaloto <maxigaloto@hotmail.com>
2022-01-23 18:28:10 -0300

    series, algunos contenidos de metadata

--- scrapweb.py
+++ scrapweb.py
@@ -7,6 +7,7 @@ import requests
 import csv
 import json
 from pprint import pprint
+import re
 
 # 2. Ejecutar URL
 
@@ -37,16 +38,12 @@ dom = BS(html, 'html.parser')
 links = dom.find_all('starz-content-item')
 
 
-# Creamos una tabla con las variables
-
-tabla_series = [['Nombre','Año', 'Episodios', 'Tipo', 'Link', 'Sinopsis']]
-
 
 #%%
 
 diccionario = []
 
-series_temporadas = []
+temporadas = []
 
 
 count = 0
@@ -55,39 +52,90 @@ for i in range(len(links)):
     url = links[i].article.div.a['href']
     url_final = 'https://www.starz.com' + url
     driver.get(url_final)
-    sleep(3)
+    sleep(4)
     html = driver.execute_script('return document.documentElement.outerHTML')
     dom = BS(html, 'html.parser')
-    sleep(6)
+    sleep(2)
     name = dom.find('div', attrs = {'class': "series-title d-flex flex-column justify-content-end"})
-    sleep(1)
+    #sleep(2)
     nombre = name.text
     datos = dom.find('ul', attrs = {'class': "meta-list text-uppercase"})
-    sleep(3)
+    #sleep(2)
     episodios = datos.find('li').find_next_sibling("li").find('span').get_text()
     tipo = datos.find('li').find_next_sibling("li").find_next_sibling("li").get_text()
     año = datos.find('li').find_next_sibling("li").find_next_sibling("li").find_next_sibling("li").get_text()
     sinopsis = dom.find('p', lines='3').text
-    print(count, '----',nombre,  '--', sinopsis)
-    caracteristicas = {'Nombre': nombre, 'Año': año, 'Episodios':episodios, 'Tipo':tipo, 'Link':url_final, 'Sinopsis':sinopsis}
+    
+    caracteristicas = {'Nombre': nombre, 'Año': año, 'Episodios':episodios, 'Tipo':tipo, 'Link':url_final, 'Sinopsis' : sinopsis}
     diccionario.append(caracteristicas) 
-    break
     
+    ver_temporadas = dom.find('div', attrs = {'class': "season-selector d-flex"})
     
-            
-    
+    ver_temporada = ver_temporadas.find('div', attrs = {'class': "season-number d-inline-block text-center active"}).text
+        
+    max_temporadas = len(ver_temporadas.text.strip('Temporada'))
+
+    print('\nCargando los datos de la serie: ',nombre, '\n')
+    len_temporadas = len(temporadas)
 
     
+    for i in range(max_temporadas):
+        url_temporadas = ver_temporadas.find_all('a')[i]['href']
+        url_temporadas_final = 'https://www.starz.com' + url_temporadas
+        driver.get(url_temporadas_final)
+        sleep(4)
+        html = driver.execute_script('return document.documentElement.outerHTML')
+        dom = BS(html, 'html.parser')
+        sleep(2)
+        capitulos_div = dom.find_all('div', attrs = {'class': "col-12 col-sm-6 col-lg-9 align-self-center"})  
+        for capitulo in capitulos_div:
+            nombre_capitulo = capitulo.find('h6', attrs = {'class': "title"}).text
+            temporada = re.search('season-(.+?)/', url_temporadas).group(1)
+            temporada = re.search('season-(.+?)/', url_temporadas).group(1)
+            minutos = capitulo.find('li').find_next_sibling("li").text
+            descripcion = capitulo.find('p', lines='2').text
+            capitulos = {'Numero': temporada, 'Capitulo': nombre_capitulo, 'Duracion' : minutos, 'Descripcion' : descripcion}
+            temporadas.append(capitulos)
+    len_temporadas_final = len(temporadas)
+    diccionario[count - 1]['Temporadas'] = list(temporadas[len_temporadas:len_temporadas_final])
+
+    
+    print('\nLa serie: ',nombre, 'contiene: ',temporada, 'temporada/s.\n')        
+    print('-------------\n')       
+    print('\nOk los datos de la serie: ',nombre)
+    print('-------------\n')
+    print('\nRestan: ', len(links) - count, ' Series')
+
+    
+print('Fin del Scrap de Series')
+
+
+
+    
+      
+    
+#%%
+
+# Guardar archivo Json
+
+with open('series.json', 'w') as archivo_json_series:
+    json.dump(diccionario, archivo_json_series, ensure_ascii=False, indent = 2)
+
+
+
+
 
 #%%
 
 
 # Prueba Series
 
+import re
+
 options = webdriver.ChromeOptions()
 options.add_argument("--incognito")
 driver = webdriver.Chrome('chromedriver.exe', options = options)
-url = 'https://www.starz.com/ar/es/series/the-borgias/61183'
+url = 'https://www.starz.com/ar/es/series/little-birds/62168'
 driver.get(url)
 
 sleep(5)
@@ -98,37 +146,39 @@ dom = BS(html, 'html.parser')
 
 
 
-
-ver_temporadas = dom.find('div', attrs = {'class': "season-selector d-flex"})
-ver_temporada = ver_temporadas.find('div', attrs = {'class': "season-number d-inline-block text-center active"}).text
-
-
-
 series_temporadas = []
 
-if ver_temporada == '1':
-    for i in range(len(ver_temporadas)):
-        sleep(1)
-        url_temporadas = ver_temporadas.find_all('a')[i]['href']
-        url_temporadas_final = 'https://www.starz.com' + url_temporadas
-        print(url_temporadas_final)
-        driver.get(url_temporadas_final)
-        sleep(3)
-        html = driver.execute_script('return document.documentElement.outerHTML')
-        dom = BS(html, 'html.parser')
-        sleep(6)
-        temporada = i + 1
-        capitulos_div = dom.find_all('div', attrs = {'class': "col-12 col-sm-6 col-lg-9 align-self-center"})  
-        for capitulo in capitulos_div:
-            nombre_capitulo = capitulo.find('h6', attrs = {'class': "title"}).text
-            temporada = temporada
-            minutos = capitulo.find('li').find_next_sibling("li").text
-            descripcion = capitulo.find('p', lines='2').text
-            caracteristicas_temporadas = {'Temporada': temporada, 'Nombre': nombre_capitulo, 'Duracion' : minutos, 'Descripcion' : descripcion}
-            series_temporadas.append(caracteristicas_temporadas) 
+ver_temporadas = dom.find('div', attrs = {'class': "season-selector d-flex"})
+max_temporadas = len(ver_temporadas.text.strip('Temporada'))
+
+
+for i in range(max_temporadas):
+    sleep(1)
+    url_temporadas = ver_temporadas.find_all('a')[i]['href']
+    url_temporadas_final = 'https://www.starz.com' + url_temporadas
+    print(url_temporadas_final)
+    driver.get(url_temporadas_final)
+    sleep(3)
+    html = driver.execute_script('return document.documentElement.outerHTML')
+    dom = BS(html, 'html.parser')
+    sleep(6)
+    temporada = re.search('season-(.+?)/', url_temporadas).group(1)
+    capitulos_div = dom.find_all('div', attrs = {'class': "col-12 col-sm-6 col-lg-9 align-self-center"})  
+    for capitulo in capitulos_div:
+        nombre_capitulo = capitulo.find('h6', attrs = {'class': "title"}).text
+        temporada = temporada
+        minutos = capitulo.find('li').find_next_sibling("li").text
+        descripcion = capitulo.find('p', lines='2').text
+        caracteristicas_temporadas = {'Temporada': temporada, 'Nombre': nombre_capitulo, 'Duracion' : minutos, 'Descripcion' : descripcion}
+        series_temporadas.append(caracteristicas_temporadas) 
+
             
 
             
+pprint(series_temporadas)
+
+
+
 
 
 
@@ -160,13 +210,20 @@ print('datos subidos')
 
 
 
+diccionario[0]['Temporadas'] = list(temporadas[1:5])
+
+
+
+temporads = []
+
+
+len(temporads)
 
 
 
 
 
-
-
+#%%
 
 
 
